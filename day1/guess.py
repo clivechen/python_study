82077079ae1b410cdc61ffc82247906f8143c27b
clive <clive@hz.cn>
2018-05-01 21:21:29 +0800

    go~

--- day1/guess.py
+++ day1/guess.py
@@ -3,11 +3,17 @@
 
 age_of_boy = 56
 
-guess_age = int(input("guess age:"))
+count = 0
+while count < 3:
+    guess_age = int(input("guess age:"))
 
-if guess_age == age_of_boy:
-    print("yes, you got it.")
-elif guess_age > age_of_boy:
-    print("think smaller...")
-else:
-    print("think bigger...")
+    if guess_age == age_of_boy:
+        print("yes, you got it.")
+        break
+    elif guess_age > age_of_boy:
+        print("think smaller...")
+    else:
+        print("think bigger...")
+    count = count +1 #count +=1
+if count == 3:
+    print("you have tried too many times...!")
--- day1/while.py
+++ day1/while.py
@@ -5,3 +5,5 @@ count = 0
 while True:
     print("count:",count)
     count = count +1 #count +=1
+    if count == 1000:
+        break
